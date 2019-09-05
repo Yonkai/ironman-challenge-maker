da42e6716d76598aa32583fd449d3a8739e8fbd8
Frank <fgunsch.webdev@gmail.com>
2019-09-05 12:54:16 -0400

    being logic abstraction in display component

--- components/displayers/AreaDisplay.js
+++ components/displayers/AreaDisplay.js
@@ -46,26 +46,7 @@ class AreaDisplay extends Component {
     }
 
     if (this.props.rootState[STATE_KEYS.AREA.USING_AREA] !== prevProps.rootState[STATE_KEYS.AREA.USING_AREA]) {
-      // Shuffle array of areas
-      const reshuffled = challenges.areas.sort(() => 0.5 - Math.random())
 
-      // figure out how much to add or remove from state array
-      const shift = this.props.rootState[STATE_KEYS.AREA.USING_AREA] - prevProps.rootState[STATE_KEYS.AREA.USING_AREA]
-
-      // Select item from shuffled array
-      const usingSelected = reshuffled.slice(0, 1)
-      if (shift > 0) {
-        // Add shuffled item to end of state, not very efficient
-        this.setState((state, props) => ({
-          areaUse: [...this.state.areaUse, (usingSelected[0])]
-        }))
-      } else if (shift < 0) {
-        var repoppedState = this.state.areaUse
-        repoppedState.pop()
-        this.setState((state, props) => ({
-          areaUse: [...repoppedState]
-        }))
-      }
     }
   }
   //   // TODO:Make stateful, storing in this comp., so you can compare previous state
