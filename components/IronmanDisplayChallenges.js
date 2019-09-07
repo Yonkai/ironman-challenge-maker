98a36c329285400a88bf030f09bfc53b3f0bf99e
Frank <fgunsch.webdev@gmail.com>
2019-09-07 18:58:01 -0400

    Remove unused state

--- components/IronmanDisplayChallenges.js
+++ components/IronmanDisplayChallenges.js
@@ -5,11 +5,6 @@ import AreaDisplay from './displayers/AreaDisplay'
 import IronmanTypeDisplay from './displayers/IronmanTypeDisplay'
 
 class IronmanDisplayChallenges extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {}
-  }
-
   render () {
     return (<div className='challengeDisplay'>
       {/* Ironman challenge condition: */}
