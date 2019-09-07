134d2e3be47b1f24d6ba0475763c958e6d4a161e
Frank <fgunsch.webdev@gmail.com>
2019-09-07 18:59:27 -0400

    Remove unused state

--- components/displayers/AbstractDisplayMaker.js
+++ components/displayers/AbstractDisplayMaker.js
@@ -3,11 +3,6 @@ import STATE_KEYS from '../../data/STATE_KEYS'
 import challenges from '../../challenges'
 
 class AbstractDisplayMaker extends Component {
-  constructor (props) {
-    super(props)
-    this.state = { nothing: '' }
-  }
-
   render () {
     return (<div><p>Nothing</p></div>)
   }
