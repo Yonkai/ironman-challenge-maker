6b255075687be0e01a3702bb09369cf1acbcad6d
Frank <fgunsch.webdev@gmail.com>
2019-09-16 23:28:58 -0400

    Add a hacky way to load display

--- components/displayers/RandomSearchDisplay.js
+++ components/displayers/RandomSearchDisplay.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react'
 import DisplayerLayoutHeader from '../../styles/DisplayerLayoutHeader.js'
-
+import STATE_KEYS from '../../data/STATE_KEYS.js'
 class RandomSearchDisplay extends Component {
   render () {
+    console.log(`${this.props.isolatedSTATE_KEYS[Object.keys(this.props.isolatedSTATE_KEYS)[0]] + STATE_KEYS.COMPOSITE_KEY_HALFS._CHALLENGE_INVENTORY}`)
     return (
       <div>
         <DisplayerLayoutHeader />
         <p>Nothing</p>
+        <div>
+
+          {this.props.rootState[`${this.props.isolatedSTATE_KEYS[Object.keys(this.props.isolatedSTATE_KEYS)[0]] + STATE_KEYS.COMPOSITE_KEY_HALFS._CHALLENGE_INVENTORY}`]
+            ? this.props.rootState[`${this.props.isolatedSTATE_KEYS[Object.keys(this.props.isolatedSTATE_KEYS)[0]] + STATE_KEYS.COMPOSITE_KEY_HALFS._CHALLENGE_INVENTORY}`].map((area, index) =>
+              <p>{`${area}`}</p>
+            ) : 0
+          }
+        </div>
         <style jsx>{``}
         </style>
       </div>
