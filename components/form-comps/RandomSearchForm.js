64bd6af6a48a2c88878046d6d6b1369b36555f78
Frank <fgunsch.webdev@gmail.com>
2019-10-17 23:52:04 -0400

    add notes

--- components/form-comps/RandomSearchForm.js
+++ components/form-comps/RandomSearchForm.js
@@ -17,6 +17,7 @@ class RandomSearchForm extends Component {
           {Object.keys(this.props.keys).map((objKey, index) => (
             <div key={index}>
               {/* WARNING: Code is setup to use 1 and -1 values specifically */}
+              {/* turn these into enum/const states */}
               <button value={1} name={this.props.keys[objKey]} onClick={(event) => this.props.handleRandomSearchChange(event, this.props.challengesKey)}>Add {this.props.keys[objKey]}</button>
               <button value={-1} name={this.props.keys[objKey]} onClick={(event) => this.props.handleRandomSearchChange(event, this.props.challengesKey)} >Remove {this.props.keys[objKey]}</button>
               <button value={2} name={this.props.keys[objKey]} onClick={(event) => this.props.handleRandomSearchChange(event, this.props.challengesKey)} >Remove ALL {this.props.keys[objKey]}</button>
