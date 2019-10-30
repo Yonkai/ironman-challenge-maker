e98557c4ec15cb71046ed4fdd4a64e86b943a872
Frank <fgunsch.webdev@gmail.com>
2019-10-30 17:58:47 -0400

    add notes

---
+++ data/dataScraper.js
@@ -0,0 +1,10 @@
+// TODO: Use this file to pull updates automatically, eventually instead of having to manually
+// scrap through the console.
+
+/*
+random notes..
+var r = [];document.querySelectorAll('tbody tr td:nth-child(3) a').forEach((i,ind)=>r[ind] = i.innerHTML);copy(r);
+https://www.google.com/search?q=cheerio+data+scraping&rlz=1C1CHBF_enUS832US832&oq=cheerio+data+scraping&aqs=chrome..69i57.3527j0j7&sourceid=chrome&ie=UTF-8
+scraping pages with node
+scraping runelite
+*/
