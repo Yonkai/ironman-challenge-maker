f6a183d3b2fad3b1a2d3f54a5ab1937153e05d7b
Frank <fgunsch.webdev@gmail.com>
2019-10-31 00:19:48 -0400

    change to css based toggle to preserve state

--- components/form-comps/RadioForm.js
+++ components/form-comps/RadioForm.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import useToggle from './hooks/useToggle.js'
 
 const RadioForm = (props) => {
@@ -7,20 +7,19 @@ const RadioForm = (props) => {
   // Should turn this wrapper into a HOC(h3 title and div):
     <div>
       <h3 onClick={toggle}>{`${props.name}🔺`}</h3>
-      {on
-        ? <div className='challenges'>
-          {/* Radio maps through dataset because that what it displays in the form
+      <div className='challenges'>
+        {/* Radio maps through dataset because that what it displays in the form
               number maps through keys. */}
-          {Object.keys(props.dataset).map((objKey, index) => (
-            <div key={index}>
-              {/* Leave here for debugging: <div>Key: {key}, Value: {props.dataset[key]}</div> */}
-              <input type='radio' id={props.dataset[objKey]} name={props.keys} value={props.dataset[objKey]}
-                onChange={(event) => props.handleChange(event)} />
-              <label htmlFor={props.dataset[objKey]}>{ props.dataset[objKey]}</label>
-            </div>
-          )
-          )}
-        </div> : <p>hidden</p> }
+        {Object.keys(props.dataset).map((objKey, index) => (
+          <div key={index}>
+            {/* Leave here for debugging: <div>Key: {key}, Value: {props.dataset[key]}</div> */}
+            <input type='radio' id={props.dataset[objKey]} name={props.keys} value={props.dataset[objKey]}
+              onChange={(event) => props.handleChange(event)} />
+            <label htmlFor={props.dataset[objKey]}>{ props.dataset[objKey]}</label>
+          </div>
+        )
+        )}
+      </div>
       <style jsx>
         {`
         h3{
@@ -28,6 +27,10 @@ const RadioForm = (props) => {
         }
         h3:hover{
           cursor:pointer;
+        }
+
+        .challenges{
+          display:${on ? 'flex' : 'none'}
         }
           }`}
       </style>
