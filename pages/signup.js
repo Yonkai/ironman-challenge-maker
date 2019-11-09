934fa0c2ae257f62700a92f2cb2c936883af69cf
Frank <fgunsch.webdev@gmail.com>
2019-11-08 23:58:48 -0500

    Add dependcy, placeholder

--- pages/signup.js
+++ pages/signup.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react'
+import { signUp } from '../styles/pageSpecificStyles.js'
 
 class SignupPage extends Component {
   render () {
     return (
-      <p>Signup</p>
+      <div>
+        <p>Signup</p>
+        <p>Signin</p>
+        <p>Request password reset token</p>
+      </div>
     )
   }
+}
+
+SignupPage.getInitialProps = async function () {
+  // define a prop for external stylesheet and then pass to _app,
+  // where it will dynamically redefine the _app shell for each
+  // page, definitely a workaround but I can't find a better
+  // solution right now.
+  return { overwrittenLayout: signUp }
 }
 
 export default SignupPage
--- styles/pageSpecificStyles.js
+++ styles/pageSpecificStyles.js
@@ -19,3 +19,13 @@ export const coolMap = css.global`
     grid-gap:0px;
     height:100vh;
   }`
+
+// Scoped styles
+export const signUp = css.global`        
+.containerMain{
+    display:grid;
+    grid-template-columns: 150px 1fr;
+    grid-template-areas:"nav map";
+    grid-gap:0px;
+    height:100vh;
+  }`
