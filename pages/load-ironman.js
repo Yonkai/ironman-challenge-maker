12a351f7e6d436af45256d4bf49cdd005a1371e2
Frank <fgunsch.webdev@gmail.com>
2019-11-23 23:42:59 -0500

    add TODO/notes

--- pages/load-ironman.js
+++ pages/load-ironman.js
@@ -11,6 +11,22 @@ class LoadIronman extends Component {
             <button>load this ironman!</button>
           </div>
         </AccountGate>
+        {/*
+            TODO: Use deviantart as a reference for the portal,
+            break it up into abstract ui components and then
+            modify the design the fit the material (ironmen) and then
+            hook it up (vernacuarly) with the backend which I haven't
+            decided yet which stack to use.
+
+            (deviantart is made with react, redux, sentry, and lodash. All of my
+            favorite stuff.)
+
+            https://www.deviantart.com/
+            https://material-ui.com/components/ (for naming conventions)
+
+            Possibily remove account gate? or at least put it on a smaller
+            section of the overall layout. Review Nir book to make it nice.
+        */}
       </>
     )
   }
