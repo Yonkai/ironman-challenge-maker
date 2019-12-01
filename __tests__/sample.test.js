5175abdfee114eeb65f5f9036698c848ff8acf28
Frank <fgunsch.webdev@gmail.com>
2019-11-30 23:08:20 -0500

    add jest.enzyme test boilerplate

---
+++ __tests__/sample.test.js
@@ -0,0 +1,17 @@
+describe('sample test 101', () => {
+  test('works as t', () => {
+    expect(1).toEqual(1)
+  })
+})
+
+describe('sample test 102', () => {
+  test('works as expected', () => {
+    expect(1).toEqual(2)
+  })
+})
+
+describe('sample test 101', () => {
+  test('works as expected', () => {
+    expect(44).toEqual(44)
+  })
+})
---
+++ __tests__/sampleTwo.test.js
@@ -0,0 +1,5 @@
+describe('sample test 101', () => {
+  test('works as expected', () => {
+    expect(1333).toEqual(1333)
+  })
+})
