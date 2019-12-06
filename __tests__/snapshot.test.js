f7ffe73cd66656b01c191ea8b401ec6414b4cd06
Frank <fgunsch.webdev@gmail.com>
2019-12-05 21:54:09 -0500

    add snapshot test example

--- __tests__/snapshot.test.js
+++ __tests__/snapshot.test.js
@@ -1,1 +1,7 @@
-// for snapshot testing
+// example snapshot
+describe(`<AAA/>`, () => {
+  it('renders and matches the snapshot', () => {
+    const price = '$220,000'
+    expect(price).toMatchSnapshot()
+  })
+})
