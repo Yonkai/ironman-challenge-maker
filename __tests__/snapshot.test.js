b794600b7ccf2b8cbfc1f7400bf8cd54a4a02f3c
Frank <fgunsch.webdev@gmail.com>
2019-12-07 19:14:04 -0500

    properly configure jest and enzyme.

--- __tests__/snapshot.test.js
+++ __tests__/snapshot.test.js
@@ -1,8 +1,11 @@
 import SignupNotice from '../components/account-auth-permissions/SignupNotice.js'
-import { shallow, mount } from 'enzyme'
-import toJson from 'enzyme-to-json'
+import Enzyme, { shallow, mount } from 'enzyme'
+import toJSON from 'enzyme-to-json'
 import React from 'react'
+import Adapter from 'enzyme-adapter-react-16'
 
+console.log(Enzyme)
+Enzyme.configure({ adapter: new Adapter() })
 // example snapshot
 describe(`<AAA/>`, () => {
   test('renders and matches the snapshot', () => {
@@ -12,10 +15,12 @@ describe(`<AAA/>`, () => {
 })
 
 // snapshot test coolMap component
-// No clue why this doesn't work....
+// No clue why this doesn't work...
 describe(`<SignupNotice/>`, () => {
-  test.skip('renders?', () => {
-    const wrapper = mount(<SignupNotice />)
-    expect(wrapper.exists()).to.equal(true)
+  test('renders???', () => {
+    console.log('FYCCCCCSDdddHKLFSDHLDSFKL;HDSFHIO;SDFSDFOHI;DSFOHHOSDFI')
+    console.log(Enzyme)
+    const wrapper = shallow(<span>FUCK THIS CODE</span>)
+    console.log(wrapper)
   })
 })
