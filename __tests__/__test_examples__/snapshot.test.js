63ebcf3d9cf2cc75a6896e37c83bcca14d86648b
Frank <fgunsch.webdev@gmail.com>
2019-12-08 23:51:56 -0500

    add notes, dependcies

--- __tests__/__test_examples__/snapshot.test.js
+++ __tests__/__test_examples__/snapshot.test.js
@@ -1,15 +1,14 @@
-import SignupNotice from '../components/account-auth-permissions/SignupNotice.js'
+import SignupNotice from '../../components/account-auth-permissions/SignupNotice.js'
 import Enzyme, { shallow, mount } from 'enzyme'
 import toJSON from 'enzyme-to-json'
 import React from 'react'
 import Adapter from 'enzyme-adapter-react-16'
-
-console.log(Enzyme)
 Enzyme.configure({ adapter: new Adapter() })
+
 // example snapshot
 describe(`<AAA/>`, () => {
   test('renders and matches the snapshot', () => {
-    const price = '$220,000'
+    const price = '$222,000'
     expect(price).toMatchSnapshot()
   })
 })
@@ -18,9 +17,6 @@ describe(`<AAA/>`, () => {
 // No clue why this doesn't work...
 describe(`<SignupNotice/>`, () => {
   test('renders???', () => {
-    console.log('FYCCCCCSDdddHKLFSDHLDSFKL;HDSFHIO;SDFSDFOHI;DSFOHHOSDFI')
-    console.log(Enzyme)
-    const wrapper = shallow(<span>FUCK THIS CODE</span>)
-    console.log(wrapper)
+    shallow(<SignupNotice />)
   })
 })
--- components/account-auth-permissions/SignupNotice.js
+++ components/account-auth-permissions/SignupNotice.js
@@ -1,3 +1,5 @@
+import React from 'react'
+
 const SignupNotice = () => {
   return (
     <div>
