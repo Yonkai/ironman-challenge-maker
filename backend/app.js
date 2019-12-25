7f7e23f4918a11499b60cf5a7a385a31f55438bb
Frank <fgunsch.webdev@gmail.com>
2019-12-24 23:55:32 -0500

    add boilerplate

--- backend/app.js
+++ backend/app.js
@@ -2,17 +2,28 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
+var helmet = require('helmet')
 
-var indexRouter = require('./routes/index');
+
+var authRouter = require('./routes/auth');
+var apiRouter = require('./routes/api');
 
 var app = express();
 
+app.use(helmet())
+
+app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+  
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/auth', authRouter);
+app.use('/api', apiRouter);
 
 module.exports = app;
